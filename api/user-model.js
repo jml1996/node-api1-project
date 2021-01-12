a61a47f6420b5512259a2e86b692612c24a66201
Joshua Lovins <joshualovins@Joshuas-MacBook-Pro.local>
2021-01-12 11:16:44 -0500

    Some comments removed (feedback from code review).

--- api/user-model.js
+++ api/user-model.js
@@ -2,33 +2,29 @@ const shortid = require('shortid')
 
 let users = [
     {
-        id: shortid.generate(), // hint: use the shortid npm package to generate it
-        name: "Jane Doe", // String, required
-        bio: "Not Tarzan's Wife, another Jane",  // String, required
+        id: shortid.generate(),
+        name: "Jane Doe",
+        bio: "Not Tarzan's Wife, another Jane", 
     },
 ]
 
 module.exports = {
   findAll() {
-    // SELECT * FROM dogs;
     return Promise.resolve(users)
   },
 
   findById(id) {
-    // SELECT * FROM dogs WHERE id = 1;
     const user = users.find(u => u.id === id)
     return Promise.resolve(user)
   },
 
   create({ name, bio }) {
-    // INSERT INTO dogs (id, name, weight, adopter_id) VALUES ('xyz', 'Foo', 10, NULL);
     const newUser = { id: shortid.generate(), name, bio }
     users.push(newUser)
     return Promise.resolve(newUser)
   },
 
   update(id, changes) {
-    // UPDATE dogs SET name = 'Foo', weight = 9, adopter_id = 'abc' WHERE id = 1;
     const user = users.find(u => u.id === id)
     if (!user) return Promise.resolve(null)
 
@@ -38,7 +34,6 @@ module.exports = {
   },
 
   delete(id) {
-    // DELETE FROM dogs WHERE id = 1;
     const user = users.find(u => u.id === id)
     if (!user) return Promise.resolve(null)
 
